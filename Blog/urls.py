86bf7f47490117261a1ea53d55756902ca4fe8a7
Rip4568 <63131796+Rip4568@users.noreply.github.com>
2022-01-22 14:24:56 -0300

    acicionado o diretorio de caminhos estaticos em urls.py no app

--- Blog/urls.py
+++ Blog/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path,include,re_path
 from .views import home,post
+from django.conf.urls.static import static
+from django.conf import settings
 urlpatterns = [
     path('',home),
     path('<int:my_id>',post),
     #re_path(r'^static/<int:my_id$',post)
-]
+] + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
